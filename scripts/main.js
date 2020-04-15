677c8e185b494581f53f5b2fb53103e6b908e4e5
Vlad <oneviddi@gmail.com>
2020-04-15 15:33:15 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    createTree(li, data[key]);
+
+    ul.append(li);
+  }
+}
+
+createTree(tree, food);
